68c4ad4f2b0c27980e6abd9cc885ff245a3d4834
hetpatel2508 <125752460+hetpatel2508@users.noreply.github.com>
2024-02-20 20:03:52 +0530

    first website using GSAP

---
+++ Simple Animation/simple_animation.js
@@ -0,0 +1,20 @@
+var time_line = gsap.timeline();
+
+time_line.from("#nav_img, #nav_options>div , #nav_buttons>div",{
+    y:-100,
+    duration:1,
+    delay:0.5,
+    opacity:0,
+    stagger:0.21,
+})
+
+time_line.from("#content>div",{
+    y:100,
+    opacity:0,
+    stagger:0.25,
+})
+
+time_line.from("#content>img",{
+    scale:0.7,
+    opacity:0,
+})
